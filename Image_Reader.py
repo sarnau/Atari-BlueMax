84cec2d76a17bac9ac38fa6b2aba4a8d65cac144
Markus Fritze <github@sarnau.com>
2025-02-15 20:18:42 +0100

    Cleanup image read, adding comments to the images

--- Image_Reader.py
+++ Image_Reader.py
@@ -41,52 +41,37 @@ atari_colors = (
 			)
 
 
-def drawImage(firstOffset=0x00,lastOffset=0x40,isColor=False,name=''):
+def drawImage(firstOffset,lastOffset,name):
 	titel = '%s ($%04x-$%04x)' % (name,firstOffset,lastOffset)
 	print(titel)
 	print('=' * len(titel))
-	if isColor:
-		width = 8
-		lineLen = 16
-	else:
-		width = 8
-		lineLen = 8
-	gap = 0
-	chheight = 8
 	for row in range(firstOffset,lastOffset):
 		bstr = '{:08b}'.format(data[row])
-		st = ''
-		if isColor:
-			for i in range(0,len(bstr),2):
-				val = int(bstr[i:i+2],2)
-				st += atari_colors[val]+PIXEL_SET*2
-			st += bcolors.DEFAULT
-		else:
-			st = bstr.replace('1',PIXEL_SET*2).replace('0',PIXEL_CLEAR*2)
+		st = bstr.replace('1',PIXEL_SET*2*2).replace('0',PIXEL_CLEAR*2*2)
 		print('%2d %s' % (row-firstOffset,st))
 	print()
 
-drawImage(0x52D7,0x52E7,False,"P0IMAGE")
-drawImage(0x52E7,0x5303,False,"P1IMAGE")
-#drawImage(0x5303,0x5308,False,"CARRT")
-#drawImage(0x5308,0x530D,False,"CARLT")
-#drawImage(0x530D,0x5312,False,"TYRRT")
-#drawImage(0x5312,0x5317,False,"TYRLT")
-#drawImage(0x5317,0x531D,False,"TRRT")
-#drawImage(0x531D,0x5323,False,"TYRTRR")
-#drawImage(0x5323,0x5329,False,"TRLT")
-#drawImage(0x5329,0x532F,False,"TYRTRL")
-drawImage(0x532F,0x5339,False,"SHDW")
-drawImage(0x5339,0x5343,False,"PLANE1")
-drawImage(0x5343,0x534B,False,"PROPIM")
-drawImage(0x538E,0x539B,False,"XPL1IM")
-#drawImage(0x53C7,0x53D3,False,"SHPIM")
-#drawImage(0x53D3,0x53DF,False,"SHPIM1")
-drawImage(0x5421,0x542B,False,"FTR")
-drawImage(0x542B,0x5436,False,"FTL")
-#drawImage(0x5436,0x543F,False,"GNST")
-drawImage(0x543F,0x5449,False,"XCRIM")
-drawImage(0x5449,0x5456,False,"XSHPIM")
-drawImage(0x5498,0x54A5,False,"AAH")
-drawImage(0x54A5,0x54B2,False,"AAI")
-drawImage(0x54F2,0x5504,False,"XCPTLIM")
+drawImage(0x52D7,0x52E7,"P0IMAGE") # regular plane (0-9), contrast color (10-15)
+drawImage(0x52E7,0x5303,"P1IMAGE") # plane shadow (0-9), plane turning left (10-16), plane turning right (17-27)
+drawImage(0x5303,0x5308,"CARRT")   # car driving right
+drawImage(0x5308,0x530D,"CARLT")   # car driving left
+drawImage(0x530D,0x5312,"TYRRT")   # car tyres driving right
+drawImage(0x5312,0x5317,"TYRLT")   # car tyres driving left
+drawImage(0x5317,0x531D,"TRRT")    # truck driving right
+drawImage(0x531D,0x5323,"TYRTRR")  # truck tyres driving right
+drawImage(0x5323,0x5329,"TRLT")    # truck driving left
+drawImage(0x5329,0x532F,"TYRTRL")  # truck tyres driving left
+drawImage(0x532F,0x5339,"SHDW")    # enemy plane shadow flying towards player
+drawImage(0x5339,0x5343,"PLANE1")  # enemy plane flying towards player
+drawImage(0x5343,0x534B,"PROPIM")
+drawImage(0x538E,0x539B,"XPL1IM")  # plane explosion
+drawImage(0x53C7,0x53D3,"SHPIM")   # moving ship
+drawImage(0x53D3,0x53DF,"SHPIM1")  # moving ship black highlight color
+drawImage(0x5421,0x542B,"FTR")
+drawImage(0x542B,0x5436,"FTL")
+drawImage(0x5436,0x543F,"GNST")    # red-white flashing bullseye target
+drawImage(0x543F,0x5449,"XCRIM")
+drawImage(0x5449,0x5456,"XSHPIM")  # moving ship explosion
+drawImage(0x5498,0x54A5,"AAH")
+drawImage(0x54A5,0x54B2,"AAI")
+drawImage(0x54F2,0x5504,"XCPTLIM")
