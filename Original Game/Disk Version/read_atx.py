5c71915a5c2c3e237eaa7915348f210b4234d5a3
Markus Fritze <github@sarnau.com>
2025-02-20 12:28:30 +0100

    Adding the disk version with the copy protection

---
+++ Original Game/Disk Version/read_atx.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import struct
+
+def loadATX(atxFilename,selectedTrack=None):
+	data = bytearray(open(atxFilename,'rb').read())
+	SECTOR_SIZE = 128
+
+	offset = 0
+
+	# read file header
+	header,version_number,fh_size = struct.unpack('<4sh22xl16x', data[offset+0:offset+0x30])
+	if header != b'AT8X':
+		print('File Header AT8X not found')
+		return False
+	if fh_size != 48:
+		print('File Header Size != 48 bytes')
+		return False
+	if version_number != 1:
+		print('File Header Version != 1')
+		return False
+	offset += fh_size
+
+	while offset < len(data):
+		# read track header
+		th_record_size,record_type,track_number,sector_count,th_size = struct.unpack('<lh2xB1xh8xl8x', data[offset+0:offset+0x20])
+		if th_size != 32:
+			print('Track Header Size != 32 bytes')
+			return False
+		if record_type != 0:
+			print('Track Header record type != data track')
+			return False
+		if not selectedTrack or (selectedTrack and selectedTrack == track_number):
+			track = data[offset+0:offset+th_record_size]
+
+			# read sector list header
+			record_size,record_type = struct.unpack('<lB3x', track[th_size+0:th_size+8])
+			if record_type != 1:
+				print('Sector List Header record type != sector list')
+				return False
+
+			# read sector list
+			for sectorHeaderOffset in range(th_size+8,th_size+record_size,8):
+				sector_number,sector_status,sector_position,start_data = struct.unpack('<BBhl', track[sectorHeaderOffset+0:sectorHeaderOffset+8])
+
+				status = []
+				if sector_status == 0x00:
+					status.append('OK')
+				else:
+					if sector_status & 0x80: # NOT READY - not valid/useful in the file
+						status.append('Reserved:80')
+					if sector_status & 0x40:
+						status.append('EXTND DATA')
+					if sector_status & 0x20:
+						status.append('deleted DAM')
+					if sector_status & 0x10:
+						status.append('RECORD NOT FOUND')
+					if sector_status & 0x08:
+						status.append('CRC ERROR')
+					if sector_status & 0x04:
+						status.append('LOST DATA')
+					if sector_status & 0x02: # DRQ - not valid/useful in the file
+						status.append('Reserved:02')
+					if sector_status & 0x01: # BUSY - not valid/useful in the file
+						status.append('Reserved:01')
+				sstr = ','.join(status)
+
+				# read sector data as well
+				sno = track_number * 18 + sector_number
+				spos = sector_position / 26042
+				print('Sector #%3d, Track #%2d Sector #%2d / %s / %7.3f%%' % (sno, track_number, sector_number,sstr,spos),end='')
+
+				if True:
+					lc = track[start_data+0]
+					for i in range(1,SECTOR_SIZE):
+						if lc != track[start_data+i]:
+							lc = -1
+							break
+					if lc >= 0:
+						print(' / $%02x * %d' % (lc, SECTOR_SIZE))
+					else:
+						print()
+						for l in range(0,SECTOR_SIZE,16):
+							print('%02x: ' % l,end='')
+							for p in range(0,16):
+								print('%02x ' % track[start_data + l + p],end='')
+							print(' ',end='')
+							for p in range(0,16):
+								c = track[start_data + l + p]
+								if c < 0x20 or c >= 0x7F:
+									c = ord('.')
+								print('%c' % c,end='')
+							print()
+				else:
+					print()
+
+			print('=' * 40)
+		# proceed to the next track
+		offset += th_record_size
+
+	return True
+
+loadATX('Blue Max (1983)(Synapse Software)(US)(Side A)[!][OS-B].atx')
